3a1ef7a6b283e31e034470cce3674b90fc38f647
Michel GILBERT <michel@treblig.fr>
2023-11-02 11:26:56 +0100

    config  roles admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -44,48 +44,53 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToRoute('Retour à CINEMAGIK', 'fa fa-home', 'app_home');
-        yield MenuItem::submenu('Articles', 'fas fa-film')
-            ->setSubItems([
-                MenuItem::linkToCrud('Tous les articles', 'fas fa-film', Post::class),
-                MenuItem::linkToCrud('Ecrire un article', 'fas fa-plus', Post::class)
-                    ->setAction(Crud::PAGE_NEW),
-            ]);
-        yield MenuItem::submenu('Réalisateurs', 'fas fa-bullhorn')
-            ->setSubItems([
-                MenuItem::linkToCrud('Tous les réalisateurs', 'fas fa-user', Director::class),
-                MenuItem::linkToCrud('Ajouter un réalisateur', 'fas fa-plus', Director::class)
-                    ->setAction(Crud::PAGE_NEW),
-            ]);
-        yield MenuItem::submenu('Acteurs', 'fas fa-user-astronaut')
-            ->setSubItems([
-                MenuItem::linkToCrud('Tous les acteurs', 'fas fa-user', Actor::class),
-                MenuItem::linkToCrud('Ajouter un acteur', 'fas fa-plus', Actor::class)
-                    ->setAction(Crud::PAGE_NEW),
-            ]);
-        yield MenuItem::submenu('Genres', 'fas fa-masks-theater')
-            ->setSubItems([
-                MenuItem::linkToCrud('Tous les genres', 'fas fa-list', Category::class),
-                MenuItem::linkToCrud('Ajouter un genre', 'fas fa-plus', Category::class)
-                    ->setAction(Crud::PAGE_NEW),
-            ]);
-        yield MenuItem::submenu('Médias', 'fas fa-image')
-            ->setSubItems([
-                MenuItem::linkToCrud('Les médias', 'fas fa-list', Media::class),
-                MenuItem::linkToCrud('Enregistrer une image', 'fas fa-plus', Media::class)
-                    ->setAction(Crud::PAGE_NEW)
-            ]);
-        yield MenuItem::submenu('Plateforme', 'fas fa-globe')
-            ->setSubItems([
-                MenuItem::linkToCrud('Toutes les plateformes', 'fas fa-list', Source::class),
-                MenuItem::linkToCrud('Ajouter une plateforme', 'fas fa-plus', Source::class)
-                    ->setAction(Crud::PAGE_NEW),
-            ]);
-        yield MenuItem::submenu('Compte utilisateurs', 'fas fa-user')
-            ->setSubItems([
-                MenuItem::linkToCrud('Tous les utilisateurs', 'fas fa-user-friends', User::class),
-                MenuItem::linkToCrud('Modifier un utilisateur', 'fas fa-plus', User::class)
-                    ->setAction(Crud::PAGE_EDIT),
-            ]);
-        yield MenuItem::linkToCrud('Commentaires', 'fas fa-comment', Comment::class);
+        if ($this->isGranted('ROLE_AUTHOR')) {
+            yield MenuItem::submenu('Articles', 'fas fa-film')
+                ->setSubItems([
+                    MenuItem::linkToCrud('Tous les articles', 'fas fa-film', Post::class),
+                    MenuItem::linkToCrud('Ecrire un article', 'fas fa-plus', Post::class)
+                        ->setAction(Crud::PAGE_NEW),
+                ]);
+            yield MenuItem::submenu('Réalisateurs', 'fas fa-bullhorn')
+                ->setSubItems([
+                    MenuItem::linkToCrud('Tous les réalisateurs', 'fas fa-user', Director::class),
+                    MenuItem::linkToCrud('Ajouter un réalisateur', 'fas fa-plus', Director::class)
+                        ->setAction(Crud::PAGE_NEW),
+                ]);
+            yield MenuItem::submenu('Acteurs', 'fas fa-user-astronaut')
+                ->setSubItems([
+                    MenuItem::linkToCrud('Tous les acteurs', 'fas fa-user', Actor::class),
+                    MenuItem::linkToCrud('Ajouter un acteur', 'fas fa-plus', Actor::class)
+                        ->setAction(Crud::PAGE_NEW),
+                ]);
+            yield MenuItem::submenu('Genres', 'fas fa-masks-theater')
+                ->setSubItems([
+                    MenuItem::linkToCrud('Tous les genres', 'fas fa-list', Category::class),
+                    MenuItem::linkToCrud('Ajouter un genre', 'fas fa-plus', Category::class)
+                        ->setAction(Crud::PAGE_NEW),
+                ]);
+            yield MenuItem::submenu('Médias', 'fas fa-image')
+                ->setSubItems([
+                    MenuItem::linkToCrud('Les médias', 'fas fa-list', Media::class),
+                    MenuItem::linkToCrud('Enregistrer une image', 'fas fa-plus', Media::class)
+                        ->setAction(Crud::PAGE_NEW)
+                ]);
+            yield MenuItem::submenu('Plateforme', 'fas fa-globe')
+                ->setSubItems([
+                    MenuItem::linkToCrud('Toutes les plateformes', 'fas fa-list', Source::class),
+                    MenuItem::linkToCrud('Ajouter une plateforme', 'fas fa-plus', Source::class)
+                        ->setAction(Crud::PAGE_NEW),
+                ]);
+        }
+
+        if ($this->isGranted('ROLE_ADMIN')) {
+            yield MenuItem::submenu('Compte utilisateurs', 'fas fa-user')
+                ->setSubItems([
+                    MenuItem::linkToCrud('Tous les utilisateurs', 'fas fa-user-friends', User::class),
+                    MenuItem::linkToCrud('Modifier un utilisateur', 'fas fa-plus', User::class)
+                        ->setAction(Crud::PAGE_EDIT),
+                ]);
+            yield MenuItem::linkToCrud('Commentaires', 'fas fa-comment', Comment::class);
+        }
     }
 }
